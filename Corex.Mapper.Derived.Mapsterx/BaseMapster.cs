9ec767e4459128c0cdfbac6bc8191fedba329d29
sametcinar <sametcinar@msn.com>
2021-02-04 23:38:11 +0300

    AutoMapper > mapster

---
+++ Corex.Mapper.Derived.Mapsterx/BaseMapster.cs
@@ -0,0 +1,13 @@
+﻿using Corex.Mapper.Infrastructure;
+using Mapster;
+
+namespace Corex.Mapper.Derived.Mapsterx
+{
+    public abstract class BaseMapster : IMapping
+    {
+        public virtual TDestination Map<TSource, TDestination>(TSource source)
+        {
+            return source.Adapt<TDestination>();
+        }
+    }
+}
---
+++ Corex.Mapper.Infrastructure/IMapping.cs
@@ -0,0 +1,8 @@
+﻿using Corex.Model.Infrastructure;
+
+namespace Corex.Mapper.Infrastructure
+{
+    public interface IMapping : ISingletonDependecy
+    {
+    }
+}
