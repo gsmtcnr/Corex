e1a97bfc6427cf89134b785caa4f4df62ce53fc7
sametcinar <sametcinar@msn.com>
2021-02-05 21:55:04 +0300

    Json Serializer > Newtonsoft

---
+++ Corex.JsonSerializer.Derived.NSoft/BaseNewtonsoftSerializer.cs
@@ -0,0 +1,26 @@
+﻿using Corex.JsonSerializer.Infrastructure;
+using Newtonsoft.Json;
+using System;
+
+namespace Corex.JsonSerializer.Derived.NSoft
+{
+    public class BaseNewtonsoftSerializer : IJsonSerializer
+    {
+        public T DeSerializeObject<T>(string data)
+        {
+            return JsonConvert.DeserializeObject<T>(data, new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+
+            });
+        }
+        public string SerializeObject<T>(T data)
+        {
+            return JsonConvert.SerializeObject(data, new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+        }
+
+    }
+}
---
+++ Corex.JsonSerializer.Infrastructure/IJsonSerializer.cs
@@ -0,0 +1,10 @@
+﻿using Corex.Model.Infrastructure;
+
+namespace Corex.JsonSerializer.Infrastructure
+{
+    public interface IJsonSerializer : ISingletonDependecy
+    {
+        string SerializeObject<T>(T data);
+        T DeSerializeObject<T>(string data);
+    }
+}
