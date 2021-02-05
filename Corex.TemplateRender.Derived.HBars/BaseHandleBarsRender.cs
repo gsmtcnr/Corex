c9fcb774b6c1dde40426a9bb79033ae2cca5f4ef
sametcinar <sametcinar@msn.com>
2021-02-05 21:42:13 +0300

    Template Render > Handle Bars

---
+++ Corex.TemplateRender.Derived.HBars/BaseHandleBarsRender.cs
@@ -0,0 +1,15 @@
+﻿using Corex.TemplateRender.Infrastructure;
+using HandlebarsDotNet;
+
+namespace Corex.TemplateRender.Derived.HBars
+{
+    public abstract class BaseHandleBarsRender : ITemplateRender
+    {
+        public string Compile(string source, object data)
+        {
+            HandlebarsTemplate<object, object> template = Handlebars.Compile(source);
+            string result = template(data);
+            return result;
+        }
+    }
+}
---
+++ Corex.TemplateRender.Infrastructure/ITemplateRender.cs
@@ -0,0 +1,9 @@
+﻿using Corex.Model.Infrastructure;
+
+namespace Corex.TemplateRender.Infrastructure
+{
+    public interface ITemplateRender : ISingletonDependecy
+    {
+        string Compile(string source, object data);
+    }
+}
