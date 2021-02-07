f08f17b727189ecd0f7bd9e9b8c55075809783cd
sametcinar <sametcinar@msn.com>
2021-02-07 16:47:15 +0300

    ExpressionSerializer

---
+++ Corex.ExpressionSerializer.Derived.SLinq/BaseExpressionSerializer.cs
@@ -0,0 +1,32 @@
+﻿using Corex.ExpressionSerializer.Infrastructure;
+using Serialize.Linq.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+
+namespace Corex.ExpressionSerializer.Derived.SLinq
+{
+    public class BaseExpressionSerializer : IExpressionSerializer
+    {
+        private readonly Serialize.Linq.Serializers.ExpressionSerializer _serializer;
+        public BaseExpressionSerializer()
+        {
+            _serializer = new Serialize.Linq.Serializers.ExpressionSerializer(new JsonSerializer());
+        }
+        public string SerializeText(Expression expression)
+        {
+
+            string value = _serializer.SerializeText(expression);
+            return value;
+        }
+        public void AddKnowType(Type type)
+        {
+            _serializer.AddKnownType(type);
+        }
+        public void AddKnowTypes(IEnumerable<Type> types)
+        {
+            _serializer.AddKnownTypes(types);
+        }
+    }
+}
---
+++ Corex.ExpressionSerializer.Infrastructure/IExpressionSerializer.cs
@@ -0,0 +1,14 @@
+﻿using Corex.Model.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Corex.ExpressionSerializer.Infrastructure
+{
+    public interface IExpressionSerializer : ISingletonDependecy
+    {
+        string SerializeText(Expression expression);
+        void AddKnowType(Type type);
+        void AddKnowTypes(IEnumerable<Type> types);
+    }
+}
