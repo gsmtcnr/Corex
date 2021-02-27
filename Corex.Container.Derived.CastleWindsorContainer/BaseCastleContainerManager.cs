5ae7cba9ba1606d47a9bbde3ba84e713eb49d535
sametcinar <sametcinar@msn.com>
2021-02-27 14:30:36 +0300

    CastleWindsorContainer

---
+++ Corex.Container.Derived.CastleWindsorContainer/BaseCastleContainerManager.cs
@@ -0,0 +1,48 @@
+﻿using Castle.MicroKernel.Lifestyle;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Corex.Container.Inftrastructure;
+using Microsoft.Extensions.Configuration;
+using System;
+
+
+namespace Corex.Container.Derived.CastleWindsorContainer
+{
+    public abstract class BaseCastleContainerManager : IContainerManager
+    {
+        private WindsorContainer _container;
+        public void Initialize(IConfigurationRoot configurationRoot)
+        {
+            _container = new WindsorContainer();
+            _container.Register(Component.For(typeof(IConfigurationRoot)).Instance(configurationRoot).LifestyleSingleton());
+            _container.Register(Classes.FromAssemblyInDirectory(new AssemblyFilter(DependecyInstaller._assemblyDirectoryName, mask: DependecyInstaller._mask))
+                  .BasedOn<IWindsorInstaller>()
+                  .WithServiceBase()
+                  .LifestyleTransient());
+            foreach (var item in _container.ResolveAll<IWindsorInstaller>())
+            {
+                _container.Install(item);
+
+            }
+        }
+        public T Resolve<T>()
+        {
+            using (BeginScope())
+            {
+                return _container.Resolve<T>();
+            }
+        }
+
+        public T[] ResolveAll<T>()
+        {
+            using (BeginScope())
+            {
+                return _container.ResolveAll<T>();
+            }
+        }
+        private IDisposable BeginScope()
+        {
+            return _container.BeginScope();
+        }
+    }
+}
---
+++ Corex.Container.Derived.CastleWindsorContainer/DependecyInstaller.cs
@@ -0,0 +1,60 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Corex.Model.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Corex.Container.Derived.CastleWindsorContainer
+{
+    internal class DependecyInstaller : IWindsorInstaller
+    {
+        internal const string _mask = "*.*";
+        internal static string _assemblyDirectoryName { get; } = string.Empty;
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+
+            AssemblyFilter assemblyFilter = new AssemblyFilter(_assemblyDirectoryName, mask: _mask);
+            RegisterSingleton(container, assemblyFilter);
+            RegisterScoped(container, assemblyFilter);
+            RegisterTransient(container, assemblyFilter);
+            RegisterTransanction(container, assemblyFilter);
+        }
+        #region Private Methods
+        private static void RegisterTransanction(IWindsorContainer container, AssemblyFilter assemblyFilter)
+        {
+            container.Register(Classes.FromAssemblyInDirectory(assemblyFilter).BasedOn<ITransactionDependecy>()
+                     .WithServiceSelect(ServiceSelector).Configure(p => p.LifestyleScoped()));
+        }
+
+        private static void RegisterTransient(IWindsorContainer container, AssemblyFilter assemblyFilter)
+        {
+            container.Register(Classes.FromAssemblyInDirectory(assemblyFilter).BasedOn<ITransientDependecy>()
+           .WithServiceSelect(ServiceSelector).LifestyleTransient());
+        }
+
+        private static void RegisterScoped(IWindsorContainer container, AssemblyFilter assemblyFilter)
+        {
+            container.Register(Classes.FromAssemblyInDirectory(assemblyFilter).BasedOn<IScopedDependency>()
+                     .WithServiceSelect(ServiceSelector).LifestyleScoped()
+                     );
+        }
+        private static void RegisterSingleton(IWindsorContainer container, AssemblyFilter assemblyFilter)
+        {
+            container.Register(
+                 Classes.FromAssemblyInDirectory(assemblyFilter)
+                     .BasedOn<ISingletonDependecy>()
+                     .WithServiceSelect(ServiceSelector).LifestyleSingleton()
+                    );
+        }
+
+        private static IEnumerable<Type> ServiceSelector(Type type, Type[] baseTypes)
+        {
+            List<Type> result = type.GetInterfaces().ToList();
+            result.Add(type);
+            return result;
+        }
+        #endregion
+    }
+}
