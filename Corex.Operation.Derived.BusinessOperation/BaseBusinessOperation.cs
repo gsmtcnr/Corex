52f8e593dfa8a9b32ad3ead990d8c170d9352fcd
sametcinar <sametcinar@msn.com>
2021-03-02 00:39:21 +0300

    updated version

--- Corex.Operation.Derived.BusinessOperation/BaseBusinessOperation.cs
+++ Corex.Operation.Derived.BusinessOperation/BaseBusinessOperation.cs
@@ -1,9 +1,10 @@
 ﻿using Corex.ExceptionHandling.Derived.Business;
 using Corex.ExceptionHandling.Infrastructure.Models;
+using Corex.Operation.Inftrastructure;
 
 namespace Corex.Operation.Derived.BusinessOperation
 {
-    public abstract class BaseBusinessOperation
+    public abstract class BaseBusinessOperation : IBusinessOperation
     {
         public virtual void NullCheck<T>(T model)
             where T : class, new()
