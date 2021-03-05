74d7d4d757394d327ee0a8f94a595760fd7525a0
sametcinar <sametcinar@msn.com>
2021-03-05 20:53:28 +0300

    CacheManagerExtensions

---
+++ Corex.Cache.Infrastructure/CacheManagerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Corex.Cache.Infrastructure
+{
+    public static class CacheManagerExtensions
+    {
+        public static T Get<T>(this ICacheManager cacheManager, string key, Func<T> acquire)
+        {
+            return Get(cacheManager, key, 5000, acquire);
+        }
+        public static T Get<T>(this ICacheManager cacheManager, string key, int cacheTime, Func<T> acquire)
+        {
+            if (cacheManager.IsSet(key))
+                return cacheManager.Get<T>(key);
+
+            T result = acquire();
+            if (cacheTime > 0)
+                cacheManager.Set(key, result, cacheTime);
+            return result;
+        }
+    }
+}
