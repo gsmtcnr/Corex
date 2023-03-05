3c0e2c21f6f213430fbc2fb9a6c2ca2f566d831b
sametcinar <sametcinar@msn.com>
2023-03-05 14:12:54 +0300

    test

--- Corex.Presentation.Derived.MVC/MvcOptions.cs
+++ Corex.Presentation.Derived.MVC/MvcOptions.cs
@@ -1,6 +1,6 @@
 ﻿namespace Corex.Presentation.Derived.MVC
 {
-    public class MvcOptions
+   public class MvcOptions
     {
         public bool UseSession { get; set; }
         public bool UseAuthentication { get; set; }
