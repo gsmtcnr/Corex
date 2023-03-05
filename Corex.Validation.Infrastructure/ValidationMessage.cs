62f78d3a49e058c21cfb81324abd7ef86a48f651
sametcinar <sametcinar@msn.com>
2023-03-05 20:20:21 +0300

    Property 1.0.3

--- Corex.Validation.Infrastructure/ValidationMessage.cs
+++ Corex.Validation.Infrastructure/ValidationMessage.cs
@@ -2,6 +2,7 @@
 {
     public class ValidationMessage 
     {
+        public string Property { get; set; }
         public string Message { get; set; }
         public string Code { get; set; }
     }
